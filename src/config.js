eaa0d85549a211536d3d05cfad991de380d35298
Siddharthg08 <guptasiddharth2000@gmail.com>
2021-07-11 13:37:07 +0530

    config path update

--- src/config.js
+++ src/config.js
@@ -9,6 +9,6 @@ const config = {
         appId: localStorage.getItem('symblAppId') || '',
         appSecret: localStorage.getItem('symblAppSecret') || ''
     },
-    appBasePath: "/" // Set this to something else if you want to deploy multiple versions on same server. Always end with /
+    appBasePath: "video/" // Set this to something else if you want to deploy multiple versions on same server. Always end with /
 };
 export default config;
